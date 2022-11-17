bcbb5214464b92dbac061c12f27913774e1b4754
Matt Moua <matmoua@gmail.com>
2022-11-17 13:18:48 -0600

    Removed bottom border from Logout in nav menu, added nav menu icons.

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { useSelector } from 'react-redux';
-import {MenuIcon, XIcon, ArrowCircleLeftIcon} from '@heroicons/react/outline'
+import {MenuIcon, XIcon, ArrowCircleLeftIcon, LogoutIcon} from '@heroicons/react/outline'
+import {HomeIcon, LocationMarkerIcon, ArchiveIcon, QuestionMarkCircleIcon} from '@heroicons/react/solid'
 
 
 
@@ -14,7 +15,7 @@ function Nav() {
   return (
     <div className='flex'>
       {user.id && 
-    <div className={'bg-white h-full w-[20%] p-5 pt-8 text-black pt-[80px] absolute  bg-zinc-300'}>
+    <div className={'bg-white h-full w-[20%] p-5 pt-8 text-black pt-[80px] absolute  bg-zinc-300 hidden md:block '}>
       <ul>
         <li>Home</li>
         <li>Location</li>
@@ -26,7 +27,7 @@ function Nav() {
     <div className='w-screen h-[80px] z-10 bg-slate-400 fixed drop-shadow-lg'>
       <div className='px-2 flex justify-between items-center w-full h-full'>
         <div className='flex items-center'>
-          <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>SAMAN</h1>
+          <h1 className='text-3xl font-bold mr-4 md:text-4xl'>SAMAN</h1>
         </div>
         {!user.id ?
         <div className='hidden md:flex pr-4'>
@@ -37,16 +38,18 @@ function Nav() {
           {!nav ? <MenuIcon className='w-5' /> : <XIcon className='w-5' /> }
         </div>
       </div>
+      {/* Mobile NavMenu */}
       <ul className={!nav ? 'hidden' : 'absolute bg-slate-400 w-full px-8'}>
-            <li className='border-b-2 border-zinc-300 w-full'>Home</li>
-            <li className='border-b-2 border-zinc-300 w-full'>Locations</li>
-            <li className='border-b-2 border-zinc-300 w-full'>Containers</li>
-            <li className='border-b-2 border-zinc-300 w-full'>About</li>
-            <li className='border-b-2 border-zinc-300 w-full'>Logout</li>
+            <li className='border-b-2 border-zinc-300 w-full flex'><HomeIcon className='w-4 mr-2' />Home</li>
+            <li className='border-b-2 border-zinc-300 w-full flex'><LocationMarkerIcon className='w-4 mr-2' />Locations</li>
+            <li className='border-b-2 border-zinc-300 w-full flex'><ArchiveIcon className='w-4 mr-2' />Containers</li>
+            <li className='border-b-2 border-zinc-300 w-full flex'><QuestionMarkCircleIcon className='w-4 mr-4'/>About</li>
+            <li className='w-full flex'><LogoutIcon className='w-4 mr-4'/>Logout</li>
+            {!user.id && 
             <div className='flex flex-col my-4'>
               <button className='bg-transparent text-orange-300 px-8 py-3 mb-4'>Sign In</button>
               <button className='px-8 py-3'>Sign Up</button>
-            </div>
+            </div> }
       </ul>
     </div>
 
